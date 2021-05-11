03bacf163f644eea78e5e2c10f783ec5307449c9
Lucy Laidlaw <lucyisabellelaidlaw@gmail.com>
2021-05-11 11:52:08 +1000

    0.2 leavess

--- my_sketch.js
+++ my_sketch.js
@@ -1,9 +1,69 @@
+let treeHeightDiv = [2, 1.5, 1.7, 1.1, 1.3];
+
+
 function setup() {
   createCanvas(windowWidth, windowHeight);
+  noStroke();
 }
 
 function draw() {
-  background(220);
-  fill(0);
-  rect(width/2, height/2, 100, 100);
+  background(color(25, 191, 128));
+
+  let spacing = windowWidth / 6;
+  let xCoord = spacing;
+  for(let i = 0; i < 5; i++) {
+    drawBaseTree(i, xCoord);
+    xCoord = xCoord + spacing
+  }
+
+  fill(color(161, 219, 5));
+  rect(0, 40, 40, windowHeight-40);
+  fill(color(101, 135, 11));
+  rect(0, mouseY, 40, windowHeight-mouseY);
+
+
+}
+
+function drawBaseTree(treeNum, xCoord) {
+  fill(color(98, 50, 57));
+  let yCoord = (windowHeight-mouseY)/treeHeightDiv[treeNum]
+  rect(xCoord, windowHeight-yCoord, 3, yCoord);
+  fill(color(161, 219, 5))
+  if(windowHeight-mouseY > 50) {
+    ellipse(xCoord-15, windowHeight-40, 30, 10);
+    ellipse(xCoord+18, windowHeight-40, 30, 10);
+  }
+  if (windowHeight-mouseY > 100) {
+    ellipse(xCoord-15, windowHeight-40, 30, 10);
+    ellipse(xCoord+18, windowHeight-40, 30, 10);
+    ellipse(xCoord-15, windowHeight-80, 30, 10);
+    ellipse(xCoord+18, windowHeight-80, 30, 10);
+  }
+
+  if (windowHeight-mouseY > 150) {
+    ellipse(xCoord-15, windowHeight-40, 30, 10);
+    ellipse(xCoord+18, windowHeight-40, 30, 10);
+    ellipse(xCoord-15, windowHeight-80, 30, 10);
+    ellipse(xCoord+18, windowHeight-80, 30, 10);
+    ellipse(xCoord-15, windowHeight-140, 30, 10);
+    ellipse(xCoord+18, windowHeight-140, 30, 10);
+  }
+
+  if (windowHeight-mouseY > 200) {
+    ellipse(xCoord-15, windowHeight-40, 30, 10);
+    ellipse(xCoord+18, windowHeight-40, 30, 10);
+    ellipse(xCoord-15, windowHeight-80, 30, 10);
+    ellipse(xCoord+18, windowHeight-80, 30, 10);
+    ellipse(xCoord-15, windowHeight-170, 30, 10);
+    ellipse(xCoord+18, windowHeight-170, 30, 10);
+  }
+
+  if (windowHeight-mouseY > 300) {
+    ellipse(xCoord-15, windowHeight-40, 30, 10);
+    ellipse(xCoord+18, windowHeight-40, 30, 10);
+    ellipse(xCoord-15, windowHeight-80, 30, 10);
+    ellipse(xCoord+18, windowHeight-80, 30, 10);
+    ellipse(xCoord-15, windowHeight-250, 30, 10);
+    ellipse(xCoord+18, windowHeight-250, 30, 10);
+  }
 }
